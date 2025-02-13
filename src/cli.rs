823f6b56d667b89107b17723df87504440e1ceba
Muzaffar Omer <muzaffar.omer@gmail.com>
2025-02-13 09:20:49 +0100

    feat: set default spaces_dir

--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,12 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 pub struct Args {
     /// Directory where the devspaces are stored
-    #[arg(short = 'd', long, value_name = "DIR")]
+    #[arg(
+        short = 'd',
+        long,
+        value_name = "DIR",
+        default_value = "/home/momer/git-worktrees"
+    )]
     pub spaces_dir: String,
 }
 
