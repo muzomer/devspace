997574da69aedabbad26a9122b2b991ed2589216
Muzaffar Omer <muzaffar.omer@gmail.com>
2025-05-27 05:17:15 +0200

    fix: push missing mod.rs

---
+++ src/components/mod.rs
@@ -0,0 +1,18 @@
+mod create_worktree;
+mod filter;
+mod list;
+mod repositories;
+mod worktrees;
+
+pub use create_worktree::CreateWorktreeComponent;
+use ratatui::style::{palette::tailwind::SLATE, Modifier, Style};
+pub use repositories::RepositoriesComponent;
+pub use worktrees::WorktreesComponent;
+
+const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
+
+#[derive(PartialEq, Debug)]
+pub enum EventState {
+    Consumed,
+    NotConsumed,
+}
